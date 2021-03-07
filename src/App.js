1114d0b95c692becc0a07c89748175b327348bd4
Hulda <huldas96@gmail.com>
2021-03-07 14:55:25 +0000

    fourth commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! This is my demo of using webhooks. I have made a webhook to connect github with netlifly so that when I push new code into the repo then netlifly starts a new build and deploys a new version of the site.
+          Hello! This is my demo of using webhooks. 
         </p>
+        <p>I have made a webhook called "GitPush" to connect github with netlify so that when I push new code into the repo then netlify starts a new build and deploys a new version of this site.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
