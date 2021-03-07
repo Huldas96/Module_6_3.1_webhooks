b2ea0d30fdef47664c11c47e2ca221c803189231
Hulda <huldas96@gmail.com>
2021-03-07 14:30:37 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! This is my demo of using webhooks. I have made a webhook to connect github with netlifly so that when I push new code into the repo then netlifly starts a new build and deploys a new version of the site.
         </p>
         <a
           className="App-link"
